445a47556cef4b6cace25c2b37f650f516ba04c5
Gamaliel Aji <132375835+OrionAji@users.noreply.github.com>
2026-03-05 21:17:32 +0100

    feature: Add search and filtering to aircraft endpoints

--- core/sorties/views.py
+++ core/sorties/views.py
@@ -7,10 +7,20 @@ from django.core.exceptions import ValidationError
 from .models import Aircraft, Pilot, Sortie
 from .serializers import AircraftSerializer, PilotSerializer, SortieSerializer
 
+from rest_framework import filters # Add this import
+from django_filters.rest_framework import DjangoFilterBackend # Add this import
+
 class AircraftViewSet(viewsets.ModelViewSet):
     queryset = Aircraft.objects.all()
     serializer_class = AircraftSerializer
+    
+    # Adding Filter and Search capabilities
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['status']
+    search_fields = ['tail_number']
 
+    # Keep your readiness_report action here too...
+    
 class PilotViewSet(viewsets.ModelViewSet):
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
